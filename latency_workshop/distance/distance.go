e33cbc8f28a7303d01990aaa5be6b72a090a32e9
Szymon Sobczak <szymon.sobczak@gmail.com>
2013-10-01 01:06:42 +0200

    distance

---
+++ latency_workshop/distance/distance.go
@@ -0,0 +1,47 @@
+// Copyright 2013 Szymon Sobczak: http://about.me/ssobczak
+// Licensed under the MIT license: http://opensource.org/licenses/MIT
+// The above copyright notice shall be included in all copies or substantial portions of the Software.
+
+package distance
+
+import (
+	"bufio"
+	"net/http"
+)
+
+func GetDistance(what, uniprot_id string) int {
+	return distance(what, fetch(uniprot_id))
+}
+
+func fetch(uniprot_id string) string {
+	resp, _ := http.Get("http://www.uniprot.org/uniprot/" + uniprot_id + ".fasta")
+	defer resp.Body.Close()
+
+	scanner := bufio.NewScanner(resp.Body)
+	scanner.Scan() // discard comment line
+
+	fasta := ""
+	for scanner.Scan() {
+		fasta += scanner.Text()
+	}
+
+	return fasta
+}
+
+func distance(what, where string) int {
+	best := len(what)
+
+	for i := 0; i != len(where)-len(what); i++ {
+		score := 0
+		for j := 0; j != len(what); j++ {
+			if what[j] != where[i+j] {
+				score++
+			}
+		}
+		if score < best {
+			best = score
+		}
+	}
+
+	return best
+}
